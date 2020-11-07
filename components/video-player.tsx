468d00afd3e8b706b7310e904697d5d96c3895c5
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-11-07 21:55:26 +0800

    Fix deploy errors

--- components/video-player.tsx
+++ components/video-player.tsx
@@ -2,7 +2,6 @@
 import { useState, useEffect, useRef } from 'react';
 import Hls from 'hls.js';
 import mux from 'mux-embed';
-import logger from '../lib/logger';
 import { breakpoints } from '../style-vars';
 
 /*
--- test/setupTests.js
+++
@@ -1,7 +0,0 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import dotenv from 'dotenv';
-
-dotenv.config({ path: '.env.test' });
-
-Enzyme.configure({ adapter: new Adapter() });
